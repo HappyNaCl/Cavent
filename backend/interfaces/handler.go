4762af9253ea0815c29aa8d8c038926a400036e6
HappyNaCl <yudhistira.fadly@gmail.com>
2025-04-11 14:06:34 +0700

    fix: logout, logout middleware

--- backend/interfaces/handler.go
+++ backend/interfaces/handler.go
@@ -1,20 +1,12 @@
 package interfaces
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"net/http"
-	"os"
-	"strings"
 
-	"github.com/HappyNaCl/Cavent/backend/application"
+	"github.com/HappyNaCl/Cavent/backend/interfaces/handler"
 	"github.com/gin-gonic/gin"
-	"github.com/markbates/goth/gothic"
 )
-
-type ContextProvider string
-const providerKey ContextProvider = "provider"
 
 func Run(port int) error{
 	r := setupRoutes()
@@ -25,17 +17,22 @@ func Run(port int) error{
 func setupRoutes() *gin.Engine{
 	r := gin.Default()
 
+	authHandler := handler.AuthHandler{}
+
 	r.GET("/", index)
 
 	auth := r.Group("/api/auth")
 	auth.Use(UnauthMiddleware())
-	auth.POST("/register", registerUser)
-	auth.POST("/login", loginCredential)
-	auth.GET("/:provider", loginWithOAuth)
-	auth.GET("/:provider/callback", loginWithOAuthCallback)
+	auth.POST("/register", authHandler.RegisterUser)
+	auth.POST("/login", authHandler.LoginCredential)
+	auth.GET("/:provider", authHandler.LoginWithOAuth)
+	auth.GET("/:provider/callback", authHandler.LoginWithOAuthCallback)
 
-	auth.Use(AuthMiddleware())
-	auth.GET("/logout", logout)
+	// auth.POST("/logout", AuthMiddleware(), authHandler.Logout)
+
+	authProtected := r.Group("/api/auth")
+	authProtected.Use(AuthMiddleware())
+	authProtected.POST("/logout", authHandler.Logout)
 
 	protected := r.Group("/api/protected")
 	protected.Use(AuthMiddleware())
@@ -55,114 +52,3 @@ func protectedIndex(c *gin.Context){
 	})
 }
 
-func loginCredential(c *gin.Context){
-	email := c.PostForm("email")
-	password := c.PostForm("password")
-	user, err := application.LoginUser(email, password)
-	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
-		return
-	}
-	token, err := application.GenerateJWT(user.Email, user.Provider)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	appDomain := os.Getenv("APP_DOMAIN")
-
-	c.SetCookie("token", token, 3600*24, "/", appDomain, false, true)
-	c.JSON(200, gin.H{
-		"message": "success",
-		"data": gin.H{
-			"provider": user.Provider,
-			"providerId": user.ProviderID,
-			"name": user.Name,
-			"email": user.Email,
-			"avatar": user.AvatarUrl,
-		},
-	})
-
-}
-
-func registerUser(c *gin.Context){
-	fullName := c.PostForm("fullName")
-	email := c.PostForm("email")
-	password := c.PostForm("password")
-
-	user, err := application.RegisterUser(fullName, email, password)
-	if err != nil {
-		if strings.Contains(strings.ToLower(err.Error()), "duplicate key") {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "User already exists"})
-			return
-		}
-
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	token, err := application.GenerateJWT(user.Email, user.Provider)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	appDomain := os.Getenv("APP_DOMAIN")
-	log.Println(appDomain)
-	c.SetCookie("token", token, 3600*24, "/", appDomain, false, true)
-
-	c.JSON(200, gin.H{
-		"message": "success",
-		"data": gin.H{
-			"provider": user.Provider,
-			"providerId": user.ProviderID,
-			"name": user.Name,
-			"email": user.Email,
-			"avatar": user.AvatarUrl,
-		},
-	})
-}
-
-func loginWithOAuth(c *gin.Context){	
-	provider := c.Param("provider")
-	c.Request = c.Request.WithContext(context.WithValue(c.Request.Context(), providerKey, provider))
-	gothic.BeginAuthHandler(c.Writer, c.Request)
-}
-
-func loginWithOAuthCallback(c *gin.Context){
-	user, err := gothic.CompleteUserAuth(c.Writer, c.Request)
-	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
-	}
-
-	token, err := application.GenerateJWT(user.Email, user.Provider)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	}
-
-	appDomain := os.Getenv("APP_DOMAIN")
-	log.Println(appDomain)
-	c.SetCookie("token", token, 3600*24, "/", appDomain, false, true)
-
-	loginUser, err := application.RegisterOrLoginOauthUser(user, user.Provider)
-	if err != nil || loginUser == nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	}
-
-	c.JSON(200, gin.H{
-		"message": "success",
-		"data": gin.H{
-			"provider": loginUser.Provider,
-			"providerId": loginUser.ProviderID,
-			"name": loginUser.Name,
-			"email": user.Email,
-			"avatar": loginUser.AvatarUrl,
-		},
-	})
-}
-
-func logout(c *gin.Context){
-	c.SetCookie("token", "", -1, "/", os.Getenv("APP_DOMAIN"), false, true)
-	c.JSON(http.StatusOK, gin.H{
-		"message": "SUCCESS",
-	})
-}
