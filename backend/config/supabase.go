fd554a832a6c7b05ddf94b2721c8459438800bc9
Yudhistira <56641228+HappyNaCl@users.noreply.github.com>
2025-05-15 16:21:13 +0700

    reset backend

--- backend/config/supabase.go
+++
@@ -1,7 +0,0 @@
-package config
-
-// func uploadToSupabase(file multipart.File, fileName string, folder string, contentType string){
-// 	fileName = fmt.Sprintf("%s/%s", folder, fileName)
-
-// 	// uploadUrl =
-// }
--- backend/interfaces/responses/success.go
+++
@@ -1,5 +0,0 @@
-package responses
-
-type SuccessResponse struct {
-	Message string      `json:"message"`
-}
